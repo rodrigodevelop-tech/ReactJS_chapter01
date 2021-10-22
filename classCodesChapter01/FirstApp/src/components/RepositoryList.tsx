4e7985b0f31cd733f672783f2c36d1df68e3003e
unknown <digoalves12@hotmail.com>
2021-10-21 21:00:00 -0300

    Codigos feito em aula do primeiro capitulo da trilha ReactJS

---
+++ classCodesChapter01/FirstApp/src/components/RepositoryList.tsx
@@ -0,0 +1,36 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+import { useState,useEffect } from "react";
+
+interface IRepository {
+  name:string;
+  description : string;
+  html_url : string;
+}
+
+export function RepositoryList(){
+
+  const [repositories, setRepositories] = useState<IRepository[]>([]);
+
+  useEffect(()=>{ 
+
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+
+  },[]);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+
+      <ul>
+        {repositories.map((repository)=>{
+          return  <RepositoryItem key={repository.name} repository={repository}/>
+        })}
+
+      </ul>
+    </section>
+  );
+}
